e2587c9496010c5fc9872fc16d0a572b36e0529b
ys101201 <ys101201@4-lab.com>
2022-09-28 18:08:49 +0900

    09/28

--- js/modalGrp.js
+++ js/modalGrp.js
@@ -1,16 +1,34 @@
-// 모달 창
-// 수조그룹생성
-let modal = document.getElementById("cGrpModal");
-let openPopup = document.getElementById("openCGrpModal");
 let closePopup = document.getElementsByClassName("closePopup")[0];
 let modalBtn = document.getElementsByClassName("modalConfBtn")[0];
 
-openPopup.addEventListener("click", () => {
-  modal.style.display = "flex";
-});
-closePopup.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-modalBtn.addEventListener("click", () => {
-  modal.style.display = "none";
-});
+if (document.getElementById("cGrpModal") !== null) {
+  // 모달 창
+  // 수조그룹생성
+  let modal = document.getElementById("cGrpModal");
+  let openPopup = document.getElementById("openCGrpModal");
+
+  openPopup.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+  closePopup.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  modalBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+} else if (document.getElementById("foodModal") !== null) {
+  // 사료급이
+  let modal2 = document.getElementById("foodModal");
+  console.log(modal2);
+  let openPopup2 = document.getElementById("openFoodModal");
+
+  openPopup2.addEventListener("click", () => {
+    modal2.style.display = "flex";
+  });
+  closePopup.addEventListener("click", () => {
+    modal2.style.display = "none";
+  });
+  modalBtn.addEventListener("click", () => {
+    modal2.style.display = "none";
+  });
+}
